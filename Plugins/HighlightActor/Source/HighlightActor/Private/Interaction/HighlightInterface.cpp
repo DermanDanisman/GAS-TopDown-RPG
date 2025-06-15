3921ed7f5a671167e3b3639efc20f490c9362881
Derman Can Danisman <dermancanwow@gmail.com>
2025-06-15 21:14:48 +0300

    Highlight Actor Plugin
    Created a plugin that will contain the logic of actor highlight when pointed.

---
+++ Plugins/HighlightActor/Source/HighlightActor/Private/Interaction/HighlightInterface.cpp
@@ -0,0 +1,7 @@
+// © 2025 Heathrow (Derman). All rights reserved.This project is the intellectual property of Heathrow (Derman) and is protected by copyright law. Unauthorized reproduction, distribution, or use of this material is strictly prohibited.Unreal Engine and its associated trademarks are used under license from Epic Games.
+
+
+#include "Interaction/HighlightInterface.h"
+
+
+// Add default functionality here for any IHighlightInterface functions that are not pure virtual.
---
+++ Plugins/HighlightActor/Source/HighlightActor/Public/Interaction/HighlightInterface.h
@@ -0,0 +1,28 @@
+// © 2025 Heathrow (Derman). All rights reserved.This project is the intellectual property of Heathrow (Derman) and is protected by copyright law. Unauthorized reproduction, distribution, or use of this material is strictly prohibited.Unreal Engine and its associated trademarks are used under license from Epic Games.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "HighlightInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE()
+class UHighlightInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class HIGHLIGHTACTOR_API IHighlightInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+
+	virtual void HighlightActor() = 0;
+	virtual void UnHighlightActor() = 0;
+};
