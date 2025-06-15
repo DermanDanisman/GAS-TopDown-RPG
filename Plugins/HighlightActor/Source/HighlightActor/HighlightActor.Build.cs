49cb7b31b14f7b02efa611e01c7c1e4b8e51c9de
Derman Can Danisman <dermancanwow@gmail.com>
2025-06-16 00:27:09 +0300

    Highlight Actors
    Created a plugin for managing highlighting actors.

--- Plugins/HighlightActor/Source/HighlightActor/HighlightActor.Build.cs
+++ Plugins/HighlightActor/Source/HighlightActor/HighlightActor.Build.cs
@@ -25,7 +25,7 @@ public class HighlightActor : ModuleRules
 		PublicDependencyModuleNames.AddRange(
 			new string[]
 			{
-				"Core",
+				"Core", "HeadMountedDisplay",
 				// ... add other public dependencies that you statically link with here ...
 			}
 			);
